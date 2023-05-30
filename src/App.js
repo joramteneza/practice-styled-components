ec65e70bea2b684d51f14c42924fef01923121ee
JoramTeneza <joram.teneza@gmail.com>
2023-05-30 10:48:20 +0800

    Initial commit: mastering styled components

---
+++ src/App.js
@@ -0,0 +1,19 @@
+import logo from "./logo.svg";
+import "./App.css";
+import Button from "./components/Button";
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <Button>Test</Button>
+      </header>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/Button/Button.js
@@ -0,0 +1,12 @@
+import styled from "styled-components";
+
+const Button = styled.button`
+  padding-left: 10rem;
+  padding-right: 10rem;
+  padding-top: 5rem;
+  padding-bottom: 5rem;
+  background-color: red;
+  color: white;
+`;
+
+export default Button;
